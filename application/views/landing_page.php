e66d0bf3de8d42b8987464cd97ae8f3a79cefce1
Riwandy <riwandys@gmail.com>
2020-03-10 02:57:23 +0700

    Basic landing page initiation

---
+++ application/views/landing_page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank Soal dan Materi ITERA</title>
+</head>
+<body>
+    <div class="lp-header">
+        <div class="header-logo">
+            <!-- Diisi dengan logo ITERA dan Tulisan BSMI -->
+            <img src="" alt=""> 
+        </div>
+        <div class="lp-navigation">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">ABOUT</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="lp-page" id="lp-1">
+        <div id="lp-1-container">
+            <h1>Selamat Datang di Website Bank Soal dan Materi ITERA</h1>
+            <h3>Dapatkan soal dan materi program studimu agar bisa menjadi referensi ketika ujian nanti</h3>
+            <a href="#">Mulai cari referensimu</a>
+        </div>
+        <img src="#" alt=""><!-- Diisi dengan gambar lampu pijar -->
+    </div>
+</body>
+</html>
