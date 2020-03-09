a20f4634e45b57223474fa851f105677d382bfad
Riwandy <riwandys@gmail.com>
2020-03-10 06:40:30 +0700

    Develop landing page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,6 +5,7 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->helper('url');
 		$this->load->view('landing_page');
 	}
 }
--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
     <title>Bank Soal dan Materi ITERA</title>
 </head>
 <body>
     <div class="lp-header">
         <div class="header-logo">
             <!-- Diisi dengan logo ITERA dan Tulisan BSMI -->
-            <img src="" alt=""> 
+            <img src="<?php echo base_url('assets/img/header_logo.png') ?>" alt="Bank Soal dan Materi Itera"> 
         </div>
         <div class="lp-navigation">
             <ul>
@@ -25,7 +26,7 @@
             <h3>Dapatkan soal dan materi program studimu agar bisa menjadi referensi ketika ujian nanti</h3>
             <a href="#">Mulai cari referensimu</a>
         </div>
-        <img src="#" alt=""><!-- Diisi dengan gambar lampu pijar -->
+        <img src="<?php echo base_url('assets/img/lp_lampu.png') ?>" alt="Welcome"><!-- Diisi dengan gambar lampu pijar -->
     </div>
 </body>
 </html>
