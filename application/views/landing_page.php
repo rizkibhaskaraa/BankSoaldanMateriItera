0a1d603e874e8a3898f85ff915ed7f509ddec729
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-04-07 22:17:04 +0700

    add File asset dan merapihkan tampilan landing page
    Merapihkan dan menambahkan file asset

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -23,8 +23,8 @@
     <div class="lp-page" id="lp-1">
         <div id="lp-1-container">
             <h1>Selamat Datang di Website<br>Bank Soal dan Materi ITERA</h1>
-            <h3>Dapatkan soal dan materi program studimu agar bisa menjadi referensi ketika ujian nanti</h3>
-            <div id="start">Mulai cari referensimu</div>
+            <h4>Dapatkan Soal dan Materi Program Studimu agar bisa menjadi referensi ketika ujian nanti</h4>
+            <div id="start">Mulai Cari Referensimu</div>
         </div>
         <div id="lp-1-container-img">
         <img src="<?php echo base_url('assets/img/lp_lampu.png') ?>" alt="Welcome"><!-- Diisi dengan gambar lampu pijar -->
@@ -33,24 +33,24 @@
     <div id="lp-about">
         <div id="container-about">
             <h1>Apa Itu Bank Soal dan <br>Materi ITERA?</h1>
-            <h3>Bank Soal dan Materi ITERA (BSMI) adalah website hasil pengembangan "INI TEAM" yang merupakan mahasiswa Teknik Informatika ITERA yang bekerja sama dengan Kementrian Riset dan Pendidikan KM ITERA 2020. BSMI ini dibuat dengan tujuaun untuk memudahkan mahasiswa ITERA dalam mencari referensi belajar</h3>
+            <h3>Bank Soal dan Materi ITERA (BSMI) merupakan Website hasil pengembangan "INI TEAM" dari Mahasiswa Teknik Informatika 17 ITERA yang bekerja sama dengan Kementrian Riset dan Pendidikan KM ITERA 2020. BSMI ini dibuat dengan tujuaun untuk memudahkan Mahasiswa ITERA dalam mencari referensi belajar</h3>
         </div>
         <div id="container-img">
-            <img src="<?php echo base_url('assets/img/lp_lampu.png') ?>" alt="Welcome"><!-- Diisi dengan gambar lampu pijar -->
+            <img src="<?php echo base_url('assets/img/asset(3).png') ?>" alt="Welcome"><!-- Diisi dengan gambar lampu pijar -->
         </div>
     </div>
     <div id="lp-about-1">
         <h1>Fasilitas yang Disediakan</h1>
         <div id="fasilitas">
         <h2>
-            <img src="<?php echo base_url('assets/img/lp_lampu.png') ?>"><br>MATERI<br>
-        menyediakan materi-materi perkuliahan dari setiap program studi yang ada di ITERA</h2>
+            <img src="<?php echo base_url('assets/img/materi.png') ?>"><br>MATERI<br>
+        Menyediakan materi-materi perkuliahan dari setiap Program Studi yang ada di ITERA</h2>
         <h2>
-            <img src="<?php echo base_url('assets/img/lp_lampu.png') ?>"><br>SOAL<br>
-        menyediakan berbagai macam soal seperti soal UTS,UAS,dan QUIS dari setiap Mata Kuliah</h2>
+            <img src="<?php echo base_url('assets/img/soal.jpg') ?>"><br>SOAL<br>
+        Terdapat berbagai macam soal seperti soal UTS,UAS,dan QUIZ dari setiap Mata Kuliah</h2>
         <h2>
-            <img src="<?php echo base_url('assets/img/lp_lampu.png') ?>"><br>VIDEO<br>
-        menyediakan tutorial video yang dapat membantu mahasiswa memahami materi dan contoh soal dari setiap Mata kuliah</h2>
+            <img src="<?php echo base_url('assets/img/video.png') ?>"><br>VIDEO<br>
+        Tutorial video yang dapat memudahkan mahasiswa memahami materi dan contoh soal dari setiap Mata kuliah</h2>
         </div>
     </div>
     <div class="form-container popup" id="formPopup">
