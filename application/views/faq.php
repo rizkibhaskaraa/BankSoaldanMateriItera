f3214ea5e681d0c894b3afd333dd6b3f249a2cc4
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-04-09 00:56:10 +0700

    Fixing
    Biar keren

--- application/views/faq.php
+++ application/views/faq.php
@@ -30,21 +30,55 @@
 
         
         <div class="content-faq">
-        <?php foreach($array as $question) {?>
-		<div class="q-a">
-			<h1 class="question">
-				<?php echo $question?>
-			</h1>
-			<p class="answer">
-				<?php echo $answer?>
-			</p>
-		</div>
-	<?php } ?>
-           
-
+                    <?php 
+                    $array = array(
+                        "Bagaimana cara menggunakan website BSMI?"=>"Kalian sebagai mahasiswa cukup search website BSMI di search engine. Setelah website ditemukan klik “Mulai cari Referensi” lalu masukkan Enrollment-Key yang akan di infokan oleh Himpunan ataupun Prodi masing-masing. Maka website BSMI sudah dapat kalian gunakan.
+                        ",
+                        "Bagaimana cara menemukan fasilitas yang tersedia di website BSMI?
+                        "=>"Setelah kalian masuk melalui Enrollment-key, kalian tinggal mencari jurusan dan prodi kalian. Setelah itu menu Materi, Soal dan Video akan muncul ",
+                        "Apakah Materi, Soal dan Video dapat di unduh?"=>"Tentu saja bisa, untuk Materi dan Soal dapat kalian unduh langsung melalui website BSMI, namun untuk Video karena menggunakan system Embed Youtube maka kalian dapat mendownload video melaui aplikasi youtube.",
+                        "Apakah dapat melakukan request Materi, Soal atau Video ?
+                        "=>"Bisa, jika kalian memiliki request content yang kalian rasa perlu ada didalam Website BSMI kalian dapat meminta operator prodi atau himpunan kalian untuk mengupload content yang kalian butuhkan.",
+                        "Apa perbedaan antara Website BSMI dan E-Learning ITERA ?"=>"Kedua website ini sama-sama memberikan kemudahan untuk mahasiswa ITERA untuk mengakses dan mencari referensi belajar yang kalian butuhkan. Namun, Website BSMI tidak memberi kalian batasan untuk mengakses materi di prodi masing-masing ataupun luar prodi dengan mengharuskan kalian enroll ke matakuliah tersebut. Jadi kalian juga bisa belajar dan menambah pengetahuan tentang luar prodi secara mudah dan praktis.");
+                    foreach($array as $question=>$answer) {?>
+                    <div class="q-a">
+                        <h1 class = "q">
+                        <?php echo "Q"?>
+                        </h1>
+                            <h1 class = "a">
+                                <?php echo "A"?>
+                            </h1>
+                        <h3 class="question">
+                            <?php echo $question?>
+                        </h3>
+                        <p class="answer">
+                            <?php echo $answer?>
+                         </p>
+                            <?php echo "<br>"?>
+                    </div>
+                <?php } ?>
            
         </div>
            
 
 </body>
+
+<footer>
+                       
+                        <div class="q-a">
+                        <h1 class = "q">
+                        <?php echo "Dikembangkan Oleh"?>
+                        </h1>
+                        </div>
+                        <center>&copy 2020. Institut Teknologi Sumatera. Hak Cipta dilindungi Undang-Undang.</center>
+
+                        <div class="footnote">
+                        <center>v06042020</center>
+                        </div>
+
+
+                    </footer>
+
 </html>
+
+
