56e8ab8eefa6b9317ed241dd8e2f93a9f0f278fe
Riwandy <riwandys@gmail.com>
2020-04-09 01:16:09 +0700

    Perbaikan tampilan halaman daftar prodi

--- application/views/halaman_prodi.php
+++ application/views/halaman_prodi.php
@@ -1,38 +1,41 @@
-    <div class="lp-page" id="lp-1">
-        <div id="lp-1-jurusan">
-        	<a href="#lp-1-sains">JURUSAN SAINS</a>
-        	<a href="#lp-1-jtpi">JURUSAN TEKNOLOGI PRODUKSI DAN INDUSTRI </a>
-        	<a href="#lp-1-jtik">JURUSAN TEKNOLOGI INFRASTRUKTUR DAN KEWILAYAHAN</a>
-        </div>
-    </div>
-    <div id="lp-1-sains">
-    	<h3>JURUSAN SAINS</h3>
-    	<div id="lp-1-prodi-sains">
-        	<?php foreach ($sains as $value) { ?>
-        		<a href="<?php echo base_url('prodi/'.$value["kode_prodi"]) ?>"> <h2><?php echo $value['nama_prodi']; ?></h2> </a>
-        	<?php } ?>
-        </div>
-    </div>
-    <div id="lp-1-jtpi">
-    	<h3>JURUSAN TEKNOLOGI PRODUKSI DAN INDUSTRI</h3>
-    	<div id="lp-1-prodi-jtpi">
-           	<?php foreach ($jtpi as $value) { ?>
-        		<a href="<?php echo base_url('prodi/'.$value["kode_prodi"]) ?>"><h2><?php echo $value['nama_prodi']; ?></h2></a>
-        	<?php } ?>
-        </div>
-    </div>
-    <div id="lp-1-jtik">
-    	<h3>JURUSAN TEKNOLOGI INFRASTRUKTUR DAN KEWILAYAHAN</h3>
-    	<div id="lp-1-prodi-jtik">
-            <?php foreach ($jtik as $value) { ?>
-        		<a href="<?php echo base_url('prodi/'.$value["kode_prodi"]) ?>"><h2><?php echo $value['nama_prodi']; ?></h2></a>
-        	<?php } ?>
-        </div>
-    </div>
-    <div id="up">
-    	<a href="#home">
-	    	<img src="<?php echo base_url('assets/img/up.png') ?>"> 
-    	</a>
-    </div>
+	<div class="container">
+		<div class="lp-page page-section jurusan-section" id="lp-1">
+		<h3>Jurusan</h3>
+			<div id="lp-1-jurusan" class="flexbox-container">
+				<a href="#lp-1-sains">Sains</a>
+				<a href="#lp-1-jtpi">Teknologi Produksi dan Industri</a>
+				<a href="#lp-1-jtik">Teknologi Infrastruktur dan Kewilayahan</a>
+			</div>
+		</div>
+		<div id="lp-1-sains" class="page-section prodi-section">
+			<h3>Jurusan Sains</h3>
+			<div id="lp-1-prodi-sains" class="flexbox-container">
+				<?php foreach ($sains as $value) { ?>
+					<a href="<?php echo base_url('prodi/'.$value["kode_prodi"]) ?>"> <h2><?php echo $value['nama_prodi']; ?></h2> </a>
+				<?php } ?>
+			</div>
+		</div>
+		<div id="lp-1-jtpi" class="page-section prodi-section">
+			<h3>Jurusan Teknologi Produksi dan Industri</h3>
+			<div id="lp-1-prodi-jtpi" class="flexbox-container">
+				<?php foreach ($jtpi as $value) { ?>
+					<a href="<?php echo base_url('prodi/'.$value["kode_prodi"]) ?>"><h2><?php echo $value['nama_prodi']; ?></h2></a>
+				<?php } ?>
+			</div>
+		</div>
+		<div id="lp-1-jtik" class="page-section prodi-section">
+			<h3>Jurusan Teknologi Infrastruktur dan Kewilayahan</h3>
+			<div id="lp-1-prodi-jtik" class="flexbox-container">
+				<?php foreach ($jtik as $value) { ?>
+					<a href="<?php echo base_url('prodi/'.$value["kode_prodi"]) ?>"><h2><?php echo $value['nama_prodi']; ?></h2></a>
+				<?php } ?>
+			</div>
+		</div>
+		<div id="up">
+			<a href="#home">
+				<img src="<?php echo base_url('assets/img/up.png') ?>"> 
+			</a>
+		</div>
+	</div>
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style2.css') ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Bank Soal dan Materi ITERA</title>
 </head>
 <body>
