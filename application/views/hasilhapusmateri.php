61e79e3b076e06e225a80b7133d0789f6045edee
aldiindrawan <45248095+aldiindrawan@users.noreply.github.com>
2020-04-09 08:47:47 +0700

    fix bug

--- application/views/hasilhapusmateri.php
+++ application/views/hasilhapusmateri.php
@@ -1,5 +1,5 @@
 <div class="lp-page" id="lp-1">
-        <div id="lp-1-prodi">
+        <div id="lp-1-prodi" class="title">
             <?php foreach ($matkul as $value) { ?>
             <h4>Materi <?php echo $value['nama_matkul']?></h4>
             <?php } ?>
@@ -14,12 +14,12 @@
         <form method="post" action="<?php echo base_url('welcome/hapusmateri/'.$kode_matkul) ?>">
 
             <!-- tombol hapus -->
-            <div id="lp-1-hapusmateri">
+            <div id="lp-1-hapusmateri" >
             <input type="submit" id="hapus" value="Hapus materi terpilih" onclick="return confirm('yakin ingin hapus?')">
             </div>
             <div style="clear: both;"></div>
 
-            <div id="lp-1-prodi-matkul">
+            <div id="lp-1-prodi-matkul" class="flexbox-container matkul-section">
            	<?php foreach ($materi as $value) { ?>
         		<a href="<?php echo base_url()?>/assets/materi/<?php echo $value["file"]?>"><h2><?php echo $value["judul"] ?></h2></a>
                 <input type="checkbox" name="pilih[]" value="<?php echo $value["kode_materi"];?>">
