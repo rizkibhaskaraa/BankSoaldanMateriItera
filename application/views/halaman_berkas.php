be09544ea39864d11c532df5a26f7f9b85c39c95
Riwandy <riwandys@gmail.com>
2020-04-30 06:47:49 +0700

    Perbaikan tampilan flexbox pada halaman pilihan jenis konten

--- application/views/halaman_berkas.php
+++ application/views/halaman_berkas.php
@@ -20,9 +20,30 @@
         </div>
         <div style="clear: both;"></div>
     	<div id="lp-1-prodi-pilihberkas" class="flexbox-container jurusan-section">
-           	<a href="<?php echo base_url('materi/'.$value["kode_matkul"])?>"><h2>Materi</h2></a>
-           	<a href="<?php echo base_url('soal/'.$value["kode_matkul"])?>"><h2>Soal</h2></a>
-           	<a href="<?php echo base_url('video/'.$value["kode_matkul"])?>"><h2>Video</h2></a>
+            <a href="<?php echo base_url('materi/'.$value["kode_matkul"])?>" class="card">
+                <div class="card-image">
+                    <img src="<?php echo base_url('assets/img/card-bg/illustration_materi.jpg') ?>" alt="<?php echo 'Materi '.$value['nama_matkul']?>">
+                </div>
+                <div class="card-title">
+                    <h2>Materi</h2>
+                </div>
+            </a>
+            <a href="<?php echo base_url('soal/'.$value["kode_matkul"])?>" class="card">
+                <div class="card-image">
+                    <img src="<?php echo base_url('assets/img/card-bg/illustration_soal.jpg') ?>" alt="<?php echo 'Soal '.$value['nama_matkul']?>">
+                </div>
+                <div class="card-title">
+                    <h2>Soal</h2>
+                </div>
+            </a>
+            <a href="<?php echo base_url('video/'.$value["kode_matkul"])?>" class="card">
+                <div class="card-image">
+                    <img src="<?php echo base_url('assets/img/card-bg/illustration_video.jpg') ?>" alt="<?php echo 'Video '.$value['nama_matkul']?>">
+                </div>
+                <div class="card-title">
+                    <h2>Video</h2>
+                </div>
+            </a>
         </div>
     </div>
 
