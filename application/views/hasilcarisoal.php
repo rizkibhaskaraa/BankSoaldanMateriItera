f4c216a7f3f4bcaa2f4d19cc76330a46afc47940
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-05-03 15:32:33 +0700

    Finishing Tampilan Search Soal
    Finishing tampilan search soal agar sama muncul card nya saat kita lakukan groupping dan search

--- application/views/hasilcarisoal.php
+++ application/views/hasilcarisoal.php
@@ -1,5 +1,12 @@
     	<div id="lp-1-prodi-carisoal">
-           	<?php foreach ($soal as $value) { ?>
-        		<a href="<?php echo base_url('assets/soal/'.$value["file"])?>"><h2><?php echo $value["judul"] ?></h2></a>
+		<?php foreach ($soal as $value) { ?>
+                <a class="card" href="<?php echo base_url('assets/soal/'.$value["file"])?>">
+                    <div class="card-image">
+                        <img src="<?php echo base_url('assets/img/card-bg/illustration_soal.jpg') ?>" alt="<?php echo $value["judul"] ?>">
+                    </div>
+                    <div class="card-title-small">
+                        <h2><?php echo $value["judul"] ?></h2>
+                    </div>
+                </a>
         	<?php } ?>
         </div>
