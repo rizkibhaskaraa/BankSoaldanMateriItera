fea837ca2ab00cfd3cd1d996b4577b4e5fde74b7
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-05-03 15:23:48 +0700

    Finishing Tampilan Cari Materi
    Memberikan finishing terhadap  tampilan cari materi karena tampilan saat disearch sebelumya berbeda

--- application/views/hasilcarimateri.php
+++ application/views/hasilcarimateri.php
@@ -1,7 +1,14 @@
 
-    	<div id="lp-1-prodi-carimatkul">
-           	<?php foreach ($materi as $value) { ?>
-        		<a href="<?php echo base_url('assets/materi/'.$value["file"])?>"><h2><?php echo $value["judul"] ?></h2></a>
+    	<div id="lp-1-prodi-carimatkul" class="flexbox-container matkul-section">
+            <?php foreach ($materi as $value) { ?>
+                <a class="card" href="<?php echo base_url('assets/materi/'.$value["file"])?>">
+                    <div class="card-image">
+                        <img src="<?php echo base_url('assets/img/card-bg/illustration_materi.jpg') ?>" alt="<?php echo $value["judul"] ?>">
+                    </div>
+                    <div class="card-title-small">
+                        <h2><?php echo $value["judul"] ?></h2>
+                    </div>
+                </a>
         	<?php } ?>
         </div>
    
